253247a5334959c3dd853a070b63fab0e036745a
amirmohammadgholampour <amirmohammadghlmpr@gmail.com>
2025-03-01 18:35:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-a = input("Name: ")
-print("Welcome"+" "+a)
+class Informations:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password 
+
+    def check_password(self):
+        if len(self.password) < 5:
+            return f"your password is short" 
+        return f""
